b38d86716615968a21f5ca8b0c0aa903bd651e0d
Samir55 <discere044@icloud.com>
2019-01-23 21:34:11 +0200

    Trials.

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -3,7 +3,26 @@ import cv2 as cv
 import numpy as np
 
 
+def delete_bad_images(train_path):
+    # Walk on data set directory.
+    for root, dirs, files in os.walk(train_path):
+        #
+        # Loop on every file in the directory.
+        #
+        for filename in files:
+            if filename[0] == '.':
+                continue
+
+            # Read image in gray scale.
+            org_img = cv.imread(train_path + '/' + filename)
+            w, h, c = org_img.shape
+
+            if w < 512 or h < 512:
+                os.remove(train_path + '/' + filename)
+
+
 def get_train_file_names(train_path):
+    files_names = []
     # Walk on data set directory.
     for root, dirs, files in os.walk(train_path):
         #
@@ -22,18 +41,29 @@ def read_training(train_path, file_names):
             continue
 
         # Read image in gray scale.
-        org_img = cv.imread(train_path + '/' + filename)
-        converted_img = cv.cvtColor(org_img, cv.COLOR_BGR2YCrCb)
+        org_img = cv.imread(train_path + '/' + filename, 0)
+        # converted_img = cv.cvtColor(org_img, cv.COLOR_BGR2YCrCb)
+        converted_img = org_img / 255.
 
-        # Scale down to hd (ground truth).
-        y = cv.resize(converted_img, (1280, 720))
+        sub_image_size = 32
+        stride = 14
+        for i in range(0, org_img.shape[0] - sub_image_size, stride):
+            if i + sub_image_size > org_img.shape[0]:
+                break
 
-        # Scale down to hd (input).
-        x = cv.resize(converted_img, (640, 360))
-        x = cv.resize(x, (1280, 720), interpolation=cv.INTER_CUBIC)
+            for j in range(0, org_img.shape[1] - sub_image_size, stride):
+                if j + sub_image_size > org_img.shape[1]:
+                    break
 
-        train_x.append(np.asarray(x[:, :, 0]).reshape(720, 1280, 1))
-        train_y.append(np.asarray(y[:, :, 0]).reshape(720, 1280, 1))
+                csub_image = converted_img[i:i + sub_image_size, j:j + sub_image_size]
+                h, w = csub_image.shape
+
+                # Scale down to (input).
+                x = cv.resize(csub_image, (h // 2, w // 2))
+                x = cv.resize(x, (h, w), interpolation=cv.INTER_CUBIC)
+
+                train_x.append(np.asarray(x[:, :]).reshape(h, w, 1))
+                train_y.append(np.asarray(csub_image[:, :]).reshape(h, w, 1))
 
     train_x = np.asarray(train_x)
     train_y = np.asarray(train_y)
--- src/model/sr_model.py
+++ src/model/sr_model.py
@@ -1,37 +1,57 @@
 # import the necessary packages
+import keras
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D
 from keras.layers.core import Activation
 from keras.optimizers import Adam
+from keras.utils import plot_model
+from keras import backend as K
 
-from src.data.data_loader import read_training, get_train_file_names
+from src.data.data_loader import read_training, get_train_file_names, delete_bad_images
 import cv2 as cv
 import numpy as np
 import time
 
-train_path = "/home/ahmedsamir/SuperResolutionVideo/data/raw/train"
+train_path = "/home/ahmedsamir/SuperResolutionVideo/data/raw/"
+model_test_path = "/home/ahmedsamir/SuperResolutionVideo/models/"
 
 
 def sr_loss():
     return None
 
 
+def PSNRLoss(y_true, y_pred):
+    """
+    PSNR is Peek Signal to Noise Ratio, which is similar to mean squared error.
+    It can be calculated as
+    PSNR = 20 * log10(MAXp) - 10 * log10(MSE)
+    When providing an unscaled input, MAXp = 255. Therefore 20 * log10(255)== 48.1308036087.
+    However, since we are scaling our input, MAXp = 1. Therefore 20 * log10(1) = 0.
+    Thus we remove that component completely and only compute the remaining MSE component.
+    """
+    return -10. * K.log(K.mean(K.square(y_pred - y_true))) / K.log(10.)
+
+
 def sr_model():
-    input_shape = [720, 1280, 1]
+    input_shape = [32, 32, 1]
 
     model = Sequential()
 
-    model.add(Conv2D(filters=64, kernel_size=(9, 9), padding="same", kernel_initializer="he_normal",
+    model.add(Conv2D(filters=64, kernel_size=(9, 9), padding="same",
+                     kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None),
                      input_shape=input_shape))
     model.add(Activation("relu"))
 
-    model.add(Conv2D(filters=32, kernel_size=(1, 1), padding="same", kernel_initializer="he_normal"))
+    model.add(Conv2D(filters=32, kernel_size=(1, 1), padding="same",
+                     kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None)))
     model.add(Activation("relu"))
 
-    model.add(Conv2D(1, kernel_size=(3, 3), padding="same", kernel_initializer="he_normal"))
+    model.add(Conv2D(1, kernel_size=(3, 3), padding="same",
+                     kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None)))
 
     # Print model layers for debugging.
     print(model.summary())
+    plot_model(model, to_file="SRCNN.png", show_shapes=True, show_layer_names=True)
 
     return model
 
@@ -39,58 +59,53 @@ def sr_model():
 def train(load_from_checkpoint=False, checkpoint=30):
     model = sr_model()
 
-    opt = Adam(lr=0.001, decay=0.001 / 10)  # decay.
-    model.compile(loss="mse", optimizer=opt)
+    opt = Adam(lr=0.001)  # decay.
+    model.compile(loss="mse", optimizer=opt, metrics=[PSNRLoss])
 
-    start = 0
     if load_from_checkpoint:
         model.load_weights("/home/ahmedsamir/SuperResolutionVideo/models/sr_model" + str(checkpoint) + ".h5")
-        start = checkpoint
 
     callbacks = []
 
     train_images = get_train_file_names(train_path)
 
-    for i in range(start + 1, len(train_images) // 200):
-        batch_start = time.clock()
-        train_x, train_y = read_training(train_path, train_images[i * 200: i * 200 + 200])
-        model.fit(train_x, train_y, batch_size=2, epochs=1, callbacks=callbacks, verbose=1)
-        if i > 0 and i % 5 == 0:
-            model.save("/home/ahmedsamir/SuperResolutionVideo/models/sr_model" + str(i) + ".h5")
+    train_x, train_y = read_training(train_path, train_images)
 
-        print("Batch of 200 took ", time.clock() - batch_start, " seconds!")
-        print(i * 200 + 200, '/' + str(len(train_images)) + " processed")
+    model.fit(train_x, train_y, batch_size=32, epochs=50, callbacks=callbacks, verbose=1)
+
+    model.save("/home/ahmedsamir/SuperResolutionVideo/models/sr_model" + ".h5")
 
 
-def predict(img_path):
+def predict(img_path, model_file_name):
     model = sr_model()
-    model.load_weights("/home/ahmedsamir/SuperResolutionVideo/models/sr_model" + str(10) + ".h5")
+    model.load_weights(model_test_path + model_file_name + ".h5")
 
     # Read image in gray scale.
     org_img = cv.imread(img_path)
+    h, w, c = org_img.shape
 
-    # Scale down to hd (input).
-    x = cv.resize(org_img, (640, 360))
-    x = cv.resize(x, (1280, 720), interpolation=cv.INTER_CUBIC)
-    org_img = x.copy()
+    down_scaled_img = cv.resize(org_img, (w // 2, h // 2), interpolation=cv.INTER_CUBIC)
+    h, w, c = down_scaled_img.shape
 
-    converted_x = cv.cvtColor(x, cv.COLOR_BGR2YCrCb)
+    up_scaled_img = cv.resize(org_img, (w * 2, h * 2), interpolation=cv.INTER_CUBIC)
+    converted_img = cv.cvtColor(up_scaled_img, cv.COLOR_BGR2YCrCb)
 
-    x = np.asarray(np.asarray(converted_x[:, :, 0]).reshape(720, 1280, 1))
+    x = np.asarray(np.asarray(converted_img[:, :, 0]).reshape(h * 2, w * 2, 1))
     x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2]))
+
     y = model.predict(x)
 
-    converted_x[:, :, 0] = y[0, :, :, 0]
-    converted_x = cv.cvtColor(converted_x, cv.COLOR_YCrCb2BGR)
+    converted_img[:, :, 0] = y[0, :, :, 0]
+    result_img = cv.cvtColor(converted_img, cv.COLOR_YCrCb2BGR)
 
-    # cv.imshow("SR", org_img)
-    # cv.waitKey(0)
+    cv.imshow("ORG", org_img)
+    cv.waitKey(0)
 
-    # cv.imshow("SR", converted_x)
-    # cv.waitKey(0)
+    cv.imshow("SR", result_img)
+    cv.waitKey(0)
 
 
+# delete_bad_images(train_path)
 train()
-# train_images = get_train_file_names(train_path)
-# predict(train_path + '/' + train_images[0])
+# predict("/home/ahmedsamir/SuperResolutionVideo/test/c.jpg", "sr_model")
 # predict("/home/ahmedsamir/SuperResolutionVideo/data/raw/1.jpg")
