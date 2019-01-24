e4c5c7fb0be7aa36ba0910408694e5d3232e6e11
Samir55 <discere044@icloud.com>
2019-01-24 21:36:14 +0200

    Trials with upconv.

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,5 +1,6 @@
 import os
 import cv2 as cv
+import keras
 import numpy as np
 
 
@@ -37,16 +38,16 @@ def read_training(train_path, file_names):
 
     for filename in file_names:
         # Ignore git ignore file.
-        if filename[0] == '.':
+        if filename[0] == '.' or filename[-3:] != 'jpg':
             continue
 
         # Read image in gray scale.
-        org_img = cv.imread(train_path + '/' + filename, 0)
+        org_img = cv.imread(train_path + '/' + filename)
         # converted_img = cv.cvtColor(org_img, cv.COLOR_BGR2YCrCb)
         converted_img = org_img / 255.
 
-        sub_image_size = 32
-        stride = 14
+        sub_image_size = 96
+        stride = 96
         for i in range(0, org_img.shape[0] - sub_image_size, stride):
             if i + sub_image_size > org_img.shape[0]:
                 break
@@ -56,16 +57,93 @@ def read_training(train_path, file_names):
                     break
 
                 csub_image = converted_img[i:i + sub_image_size, j:j + sub_image_size]
-                h, w = csub_image.shape
+                h, w, c = csub_image.shape
 
                 # Scale down to (input).
                 x = cv.resize(csub_image, (h // 2, w // 2))
-                x = cv.resize(x, (h, w), interpolation=cv.INTER_CUBIC)
+                h, w, c = x.shape
+                # x = cv.resize(x, (h, w), interpolation=cv.INTER_CUBIC)
 
-                train_x.append(np.asarray(x[:, :]).reshape(h, w, 1))
-                train_y.append(np.asarray(csub_image[:, :]).reshape(h, w, 1))
+                train_x.append(np.asarray(x[:, :]).reshape(h, w, 3))
+                train_y.append(np.asarray(csub_image[:, :]).reshape(h * 2, w * 2, 3))
 
     train_x = np.asarray(train_x)
     train_y = np.asarray(train_y)
 
     return train_x, train_y
+
+
+class UPCONVDataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+
+    def __init__(self, data_path, file_names, batch_size=5, shuffle=True):
+        # Initialization
+        self.batch_size = batch_size
+        self.filenames = file_names
+        self.data_path = data_path
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        # Denotes the number of batches per epoch
+        return int(np.floor(len(self.filenames) / self.batch_size))
+
+    def __getitem__(self, index):
+        # Generate one batch of data
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Find list of IDs
+        file_names_list = [self.filenames[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(file_names_list)
+
+        return X, y
+
+    def on_epoch_end(self):
+        # Updates indexes after each epoch
+        self.indexes = np.arange(len(self.filenames))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, file_names_list):
+        # Generates data containing batch_size samples
+        X = []
+        y = []
+
+        for filename in file_names_list:
+            # Ignore git ignore file.
+            if filename[0] == '.' or filename[-3:] != 'jpg':
+                continue
+
+            # Read image in gray scale.
+            org_img = cv.imread(self.data_path + '/' + filename)
+            # converted_img = cv.cvtColor(org_img, cv.COLOR_BGR2YCrCb)
+            converted_img = org_img / 255.
+
+            sub_image_size = 96
+            stride = 96
+            for i in range(0, org_img.shape[0] - sub_image_size, stride):
+                if i + sub_image_size > org_img.shape[0]:
+                    break
+
+                for j in range(0, org_img.shape[1] - sub_image_size, stride):
+                    if j + sub_image_size > org_img.shape[1]:
+                        break
+
+                    csub_image = converted_img[i:i + sub_image_size, j:j + sub_image_size]
+                    h, w, c = csub_image.shape
+
+                    # Scale down to (input).
+                    x = cv.resize(csub_image, (h // 2, w // 2))
+                    h, w, c = x.shape
+                    # x = cv.resize(x, (h, w), interpolation=cv.INTER_CUBIC)
+
+                    X.append(np.asarray(x[:, :]).reshape(h, w, 3))
+                    y.append(np.asarray(csub_image[:, :]).reshape(h * 2, w * 2, 3))
+
+        X = np.asarray(X)
+        y = np.asarray(y)
+
+        return X, y
--- src/model/sr_model.py
+++ src/model/sr_model.py
@@ -1,20 +1,21 @@
 # import the necessary packages
 import keras
 from keras.models import Sequential
-from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
 from keras.layers.core import Activation
 from keras.optimizers import Adam
 from keras.utils import plot_model
 from keras import backend as K
 
-from src.data.data_loader import read_training, get_train_file_names, delete_bad_images
+from src.data.data_loader import read_training, get_train_file_names, UPCONVDataGenerator
 import cv2 as cv
 import numpy as np
 import time
 
 train_path = "/home/ahmedsamir/SuperResolutionVideo/data/raw/"
+upconv_train_path = "/home/ahmedsamir/Downloads/ukbench/ukbench/full/"
 model_test_path = "/home/ahmedsamir/SuperResolutionVideo/models/"
-NUM_EPOCHES = 20
+NUM_EPOCHS = 20
 
 
 def sr_loss():
@@ -33,80 +34,162 @@ def PSNRLoss(y_true, y_pred):
     return -10. * K.log(K.mean(K.square(y_pred - y_true))) / K.log(10.)
 
 
-def sr_model():
-    input_shape = [None, None, 3]
+class SRCNN:
+    def sr_model(self):
+        input_shape = [None, None, 1]
 
-    model = Sequential()
+        model = Sequential()
 
-    model.add(Conv2D(filters=64, kernel_size=(9, 9), padding="same",
-                     kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None),
-                     input_shape=input_shape))
-    model.add(Activation("relu"))
+        model.add(Conv2D(filters=64, kernel_size=(9, 9), padding="same",
+                         kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None),
+                         input_shape=input_shape))
+        model.add(Activation("relu"))
 
-    model.add(Conv2D(filters=32, kernel_size=(1, 1), padding="same",
-                     kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None)))
-    model.add(Activation("relu"))
+        model.add(Conv2D(filters=32, kernel_size=(1, 1), padding="same",
+                         kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None)))
+        model.add(Activation("relu"))
 
-    model.add(Conv2D(1, kernel_size=(3, 3), padding="same",
-                     kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None)))
+        model.add(Conv2D(1, kernel_size=(3, 3), padding="same",
+                         kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=None)))
 
-    # Print model layers for debugging.
-    print(model.summary())
-    plot_model(model, to_file="SRCNN.png", show_shapes=True, show_layer_names=True)
+        # Print model layers for debugging.
+        print(model.summary())
+        plot_model(model, to_file="SRCNN.png", show_shapes=True, show_layer_names=True)
 
-    return model
+        return model
 
+    def train(self, load_from_checkpoint=False, checkpoint=30):
+        model = self.sr_model()
 
-def train(load_from_checkpoint=False, checkpoint=30):
-    model = sr_model()
+        opt = Adam(lr=0.001)  # decay.
+        model.compile(loss="mse", optimizer=opt, metrics=[PSNRLoss])
 
-    opt = Adam(lr=0.001)  # decay.
-    model.compile(loss="mse", optimizer=opt, metrics=[PSNRLoss])
+        if load_from_checkpoint:
+            model.load_weights("/home/ahmedsamir/SuperResolutionVideo/models/sr_model" + str(checkpoint) + ".h5")
 
-    if load_from_checkpoint:
-        model.load_weights("/home/ahmedsamir/SuperResolutionVideo/models/sr_model" + str(checkpoint) + ".h5")
+        callbacks = []
 
-    callbacks = []
+        train_images = get_train_file_names(train_path)
 
-    train_images = get_train_file_names(train_path)
+        train_x, train_y = read_training(train_path, train_images)
 
-    train_x, train_y = read_training(train_path, train_images)
+        model.fit(train_x, train_y, batch_size=32, epochs=NUM_EPOCHS, callbacks=callbacks, verbose=1)
 
-    model.fit(train_x, train_y, batch_size=32, epochs=NUM_EPOCHES, callbacks=callbacks, verbose=1)
+        model.save("/home/ahmedsamir/SuperResolutionVideo/models/sr_model" + ".h5")
 
-    model.save("/home/ahmedsamir/SuperResolutionVideo/models/sr_model" + ".h5")
+    def predict(self, img_path, model_file_name):
+        model = self.sr_model()
+        model.load_weights(model_test_path + model_file_name + ".h5")
 
+        # Read image in gray scale.
+        org_img = cv.imread(img_path)
+        h, w, c = org_img.shape
 
-def predict(img_path, model_file_name):
-    model = sr_model()
-    model.load_weights(model_test_path + model_file_name + ".h5")
+        down_scaled_img = cv.resize(org_img, (w // 2, h // 2), interpolation=cv.INTER_CUBIC)
+        h, w, c = down_scaled_img.shape
 
-    # Read image in gray scale.
-    org_img = cv.imread(img_path)
-    h, w, c = org_img.shape
+        up_scaled_img = cv.resize(down_scaled_img, (w * 2, h * 2), interpolation=cv.INTER_CUBIC)
+        converted_img = cv.cvtColor(up_scaled_img, cv.COLOR_BGR2YCrCb)
 
-    down_scaled_img = cv.resize(org_img, (w // 2, h // 2), interpolation=cv.INTER_CUBIC)
-    h, w, c = down_scaled_img.shape
+        x = np.asarray(np.asarray(converted_img[:, :, 0]).reshape(h * 2, w * 2, 1))
+        x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2]))
 
-    up_scaled_img = cv.resize(down_scaled_img, (w * 2, h * 2), interpolation=cv.INTER_CUBIC)
-    converted_img = cv.cvtColor(up_scaled_img, cv.COLOR_BGR2YCrCb)
+        y = model.predict(x)
 
-    x = np.asarray(np.asarray(converted_img[:, :, 0]).reshape(h * 2, w * 2, 1))
-    x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2]))
+        converted_img[:, :, 0] = y[0, :, :, 0]
+        result_img = cv.cvtColor(converted_img, cv.COLOR_YCrCb2BGR)
 
-    y = model.predict(x)
+        cv.imshow("ORG", up_scaled_img)
+        cv.waitKey(0)
 
-    converted_img[:, :, 0] = y[0, :, :, 0]
-    result_img = cv.cvtColor(converted_img, cv.COLOR_YCrCb2BGR)
-
-    cv.imshow("ORG", up_scaled_img)
-    cv.waitKey(0)
-
-    cv.imshow("SR", result_img)
-    cv.waitKey(0)
+        cv.imshow("SR", result_img)
+        cv.waitKey(0)
 
 
-# delete_bad_images(train_path)
-train()
-predict("/home/ahmedsamir/SuperResolutionVideo/test/c.jpg", "sr_model")
-# predict("/home/ahmedsamir/SuperResolutionVideo/data/raw/1.jpg")
+class UPCONV:
+    NUM_CHANNELS = 3
+    NUM_EPOCHS = 5
+    BATCH_SIZE = 32
+
+    def upconv_model(self):
+        input_shape = [None, None, UPCONV.NUM_CHANNELS]
+
+        model = Sequential()
+
+        model.add(Conv2D(filters=16, kernel_size=(3, 3), padding="same", input_shape=input_shape))
+        model.add(keras.layers.LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(filters=32, kernel_size=(3, 3), padding="same"))
+        model.add(keras.layers.LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same"))
+        model.add(keras.layers.LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same"))
+        model.add(keras.layers.LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same"))
+        model.add(keras.layers.LeakyReLU(alpha=0.1))
+
+        model.add(Conv2DTranspose(UPCONV.NUM_CHANNELS, kernel_size=(3, 3), strides=2, padding='same', use_bias=False))
+
+        # Print model layers for debugging.
+        print(model.summary())
+        plot_model(model, to_file="UPCONV.png", show_shapes=True, show_layer_names=True)
+        return model
+
+    def train(self):
+        model = self.upconv_model()
+
+        opt = Adam(lr=0.001)  # decay.
+        model.compile(loss="mse", optimizer=opt, metrics=[PSNRLoss])
+
+        callbacks = []
+
+        train_images = get_train_file_names(upconv_train_path)
+
+        # training_generator = UPCONVDataGenerator(upconv_train_path, train_images)
+        # model.fit_generator(generator=training_generator)
+
+        for e in range(UPCONV.NUM_EPOCHS):
+            t = time.clock()
+            for i in range(len(train_images) // UPCONV.BATCH_SIZE):
+                train_x, train_y = read_training(upconv_train_path,
+                                                 train_images[i * UPCONV.BATCH_SIZE: (i + 1) * UPCONV.BATCH_SIZE])
+                model.fit(train_x, train_y, batch_size=32, epochs=1)
+            print("ONE EPOCH time is " + str(time.clock() - t))
+            model.save("/home/ahmedsamir/SuperResolutionVideo/models/upconv_model" + ".h5")
+
+    def predict(self, img_path, model_file_name):
+        model = self.upconv_model()
+        model.load_weights(model_test_path + model_file_name + ".h5")
+
+        # Read image in gray scale.
+        org_img = cv.imread(img_path)
+        h, w, c = org_img.shape
+
+        down_scaled_img = cv.resize(org_img, (w // 2, h // 2), interpolation=cv.INTER_CUBIC)
+        x = np.asarray(np.asarray(down_scaled_img))
+        x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2]))
+        h, w, c = down_scaled_img.shape
+
+        up_scaled_img = cv.resize(down_scaled_img, (w * 2, h * 2), interpolation=cv.INTER_CUBIC)
+
+        y = model.predict(x)
+
+        result_img = y[0, :, :, :]
+
+        cv.imshow("UP_SCALED", up_scaled_img)
+        cv.waitKey(0)
+
+        cv.imshow("UP_CONV", result_img)
+        cv.waitKey(0)
+
+
+# SRCNN
+# SRCNN().train()
+# SRCNN().predict("/home/ahmedsamir/SuperResolutionVideo/test/c.jpg", "sr_model")
+
+# UPCONV
+UPCONV().train()
+# UPCONV().predict("/home/ahmedsamir/SuperResolutionVideo/test/c.jpg", "upconv_model")
